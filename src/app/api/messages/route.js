881fd85bcae0c99d3cc151c2bc1fe0b3b24435a8
vanuraag27 <a297020625@gmail.com>
2026-07-22 11:16:21 +0530

    Fix import path for connectToDatabase in messages API route

--- src/app/api/messages/route.js
+++ src/app/api/messages/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { connectToDatabase } from '@/lib/mongodb';
+import { connectToDatabase } from '../../../lib/mongodb';
 import { ObjectId } from 'mongodb';
 
 // GET: Fetch conversation OR unread message indicators
